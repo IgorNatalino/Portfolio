7efe3b6adff277a4bdae42a49555e9df4ab38831
Igor Natalino <IgorNatalino@users.noreply.github.com>
2024-04-02 13:56:19 -0300

    changes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// MENU HAMBURGUER
+
 const menuHamburguer = document.querySelector('.cabecalho_menu');
 const menuLista = document.querySelector('.cabecalho_menu_lista');
 
@@ -7,3 +9,29 @@ function abreMenu() {
 
 // Adiciona um evento de clique ao elemento menuHamburguer
 menuHamburguer.addEventListener('click', abreMenu);
+
+// DE VOLTA AO TOPO
+
+let mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+	AparecerBotao();
+};
+
+function AparecerBotao() {
+	// Pixels para o botão aparecer
+	if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
+}
+
+mybutton.addEventListener('click', VoltoAoTopo());
+
+function VoltoAoTopo() {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	});
+}
