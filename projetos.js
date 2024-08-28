7b5159259031503f5080994ab62a6245852b0acb
Igor Natalino <igornatalino@gmail.com>
2024-08-28 13:53:08 -0300

    Projetos gerados por JS

---
+++ projetos.js
@@ -0,0 +1,71 @@
+const projetos = [
+	{
+		imagem: 'assets/projetos/socialmedialinks_preview.png',
+		titulo: 'Links de Social Media',
+		descricao: 'Projeto agregador de Links, desenvolvido com foco no Tailwind',
+		tecnologias: [
+			{
+				nome: 'HTML5',
+				icone: `<img src="assets/Icons/html-5-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="HTML5 Logo">`,
+			},
+			{
+				nome: 'CSS3',
+				icone: `<img src="assets/Icons/css-3-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="CSS Logo">`,
+			},
+			{
+				nome: 'Tailwind',
+				icone: `<img src="assets/Icons/tailwind-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Tailwind Logo">`,
+			},
+		],
+		links: {
+			site: ``,
+			code: ``,
+		},
+	},
+];
+
+/* <img src="assets/Icons/html-5-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="HTML5 Logo">
+
+<img src="assets/Icons/css-3-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="CSS Logo">
+
+<img src="assets/Icons/js-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Javascript Logo">
+
+<img src="assets/Icons/sass-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Sass Logo">
+
+<img src="assets/Icons/git-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Git Logo">
+
+<img src="assets/Icons/adobe-photoshop-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Photoshop Logo">
+
+<img src="assets/Icons/figma-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Figma Logo">
+
+<img src="assets/Icons/nodejs-icon-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Node JS Logo">
+
+<img src="assets/Icons/tailwind-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Tailwind Logo">
+
+<img src="assets/Icons/wordpress-icon-logo-svgrepo-com.svg" loading="lazy"
+class="projetos_skills_img" alt="Wordpress Logo">
+
+<img src="assets/Icons/typescript-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Typescript logo">
+
+<img src="assets/Icons/mysql-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="MySQL logo">
+
+<img src="assets/Icons/angular-svgrepo-com.svg" loading="lazy" class="projetos_skills_img"
+alt="Angular logo">
+
+<img src="assets/Icons/aws-svgrepo-com -branco.svg" loading="lazy" class="projetos_skills_img"
+alt="AWS logo">
+*/
